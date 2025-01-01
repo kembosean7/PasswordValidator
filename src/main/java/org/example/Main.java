f689312f3499e6b821b3dbf3e54cee0e46568089
Sean Kembo <kembosean7@gmail.com>
2025-01-01 21:56:00 +0200

    taking user input of the password

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter password to validate: ");
+        String password = scanner.nextLine();
 
         validatePassword validatePassword = new validatePassword();
-        boolean output = validatePassword.isPasswordSecure("seaS2425_n");
-        System.out.println(output);
+        boolean output = validatePassword.isPasswordSecure(password);
+        System.out.println("Is the password secure? " + output);
     }
 }
